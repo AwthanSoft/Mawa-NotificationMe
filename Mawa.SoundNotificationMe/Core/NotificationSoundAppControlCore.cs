c8de7386cc929a677c3bb029b557218709dd2d95
AwthanSoft <softmawa@gmail.com>
2021-10-01 18:35:29 +0300

    Add SoundNotification project

---
+++ Mawa.SoundNotificationMe/Core/NotificationSoundAppControlCore.cs
@@ -0,0 +1,92 @@
+﻿using CommonAppCore.Locks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mawa.SoundNotificationMe
+{
+    public abstract class NotificationSoundAppControlCore : INotificationSoundApp, IDisposable
+    {
+        #region Initail
+
+        public NotificationSoundAppControlCore()
+        {
+            pre_refresh();
+
+            NotificationSoundPasser.instance = this;
+        }
+        private void pre_refresh()
+        {
+            objectLock = new ObjectLock();
+        }
+
+        // For Lock
+        #region Lock Base Properties
+        private ObjectLock objectLock;
+
+        protected void open_lock()
+        {
+            objectLock.open_lock();
+        }
+        protected void close_lock()
+        {
+            objectLock.close_lock();
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Shower
+
+        public virtual void GeneralNotify()
+        {
+            open_lock();
+            _GeneralNotify();
+            close_lock();
+        }
+        protected abstract void _GeneralNotify();
+
+        #endregion
+
+        #region Dispose
+
+        private bool _disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                // Free any other managed objects here.
+                this?.Dispose_FreeManaged();
+            }
+
+            // Free any unmanaged objects here.
+            this?.Dispose_FreeUnManaged();
+
+
+            _disposed = true;
+        }
+
+        protected abstract void Dispose_FreeManaged();
+        protected abstract void Dispose_FreeUnManaged();
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+       
+
+        ~NotificationSoundAppControlCore()
+        {
+            Dispose(false);
+        }
+       
+
+        #endregion
+    }
+}
---
+++ Mawa.SoundNotificationMe/Core/NotificationSoundPasser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mawa.SoundNotificationMe
+{
+    public static class NotificationSoundPasser //: IMessageShowerApp
+    {
+        private static NotificationSoundAppControlCore _instance;
+        internal static NotificationSoundAppControlCore instance
+        {
+             set
+            {
+                if (_instance == null)
+                    _instance = value;
+                else
+                    throw new Exception();
+            }
+            //get => _instance;
+            // as temp for test .?
+            get
+            {
+                if (_instance == null)
+                    throw new Exception();
+                return _instance;
+            }
+        }
+
+        public static void GeneralNotify()
+        {
+            instance?.GeneralNotify();
+        }
+    }
+}
